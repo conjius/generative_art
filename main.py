5fac805b2adf4879bc789eb8a1c51290510c2562
Dais <daispaysagist@gmail.com>
2020-02-01 13:09:18 +0200

    preparing merge to master

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from canvas import Canvas
 from leaf import LeafDrawer
 
-num_rows = 5
-num_cols = 5
+num_rows = 4
+num_cols = 4
 canvas_size = (1280, 1280)
 
 
@@ -14,7 +14,7 @@ def main():
     # leaf_drawer.draw_single_leaf()
 
     # generate a grid of leaves
-    leaf_drawer.draw_multiple_leaves(num_rows=num_rows, num_cols=num_cols, save_to_file=False)
+    leaf_drawer.draw_multiple_leaves(num_rows=num_rows, num_cols=num_cols, save_to_file=True)
 
 
 if __name__ == "__main__":
